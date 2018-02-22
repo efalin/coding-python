a0a8371a91f298eb4e51dd97d55409cf83c96e5c
efalin <emiline.falin@gmail.com>
2018-02-22 11:16:07 -0500

    homework #1) git push git push variables.py git commit -m homework

---
+++ variables.py
@@ -0,0 +1,24 @@
+#x = 3
+
+#y = float( input("Type your favorite number: "))
+
+#z = x * y
+
+
+
+#print("ex is cool cause it equals: {0}, but y isn't cause it is {1}".format(x, y))
+
+#print("y: {0}".format(y))
+
+#print("z: {0}".format(z))
+
+
+
+# Homework
+# Write a program that asks for a number and prints the square of that number
+# the number might be a decimal number, it will always be in base 10
+# Store the answer in a variable before printing it.
+
+x = float( input("Type your favorite number:"))
+y = x * x
+print ("The square of your number is: {0}".format(y))

63cdc3e3210553c176439abda61100e4b3bce39e
efalin <emiline.falin@gmail.com>
2018-02-23 13:12:36 -0500

    Averting Missiles

---
+++ Missiles.py
@@ -0,0 +1,8 @@
+
+y = float(input("Hey there, French guy! How many Kilometers away is the missile?"))
+z = y * 0.621371
+if z <= 3:
+	print ("Fire away! You would most likely hit it and avert disaster... today, anyway.")
+else:
+	print ("Don't fire the missile yet. It would be absolutly pointless.")
+

bde6176ed1bbd1f8d18c4c1c64aaf9840f1a193c
efalin <emiline.falin@gmail.com>
2018-03-07 12:08:36 -0500

    updated for papa

--- password.py
+++ password.py
@@ -6,10 +6,11 @@ print("SECURE LOGIN SYSTEM")
 
 while not goodanswer:
 	answer = int( input("Enter Password: " ) )
-if answer > password:
-	print("Access Denied – Your entry was too high")
-elif answer < password:
-	answer < password print("Access Denied – Your entry was too low")
+	
+	if answer > password:
+		print("Access Denied – Your entry was too high")
+	elif answer < password:
+		print("Access Denied – Your entry was too low")
 
 else:
 	print("Access Granted")

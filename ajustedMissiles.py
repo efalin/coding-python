ac5743ad687fe8907282b7ee7151c01750552b3b
efalin <emiline.falin@gmail.com>
2018-03-16 12:15:54 -0400

    can now ask for miles and kilometers

--- ajustedMissiles.py
+++ ajustedMissiles.py
@@ -1,17 +1,17 @@
 
-measure = string(input("Is it in miles 'mi' or kilometers 'km'?"))
-if measure = ("km"):
-	y = float(input("So how many Kilometers away is the missile?"))
+measure = input("Is it in miles 'mi' or kilometers 'km'? ")
+if measure == "km":
+	y = float(input("So how many Kilometers away is the missile? "))
 	z = y * 0.621371
-		if z <= 3:
-			print ("Fire away! You would most likely hit it and avert disaster... today, anyway.")
-		else:
-			print ("Don't fire the missile yet. It would be absolutly pointless.")
+	if z <= 3:
+		print ("Fire away! You would most likely hit it and avert disaster... today, anyway.")
 	else:
-			mi = float(input("So how many miles away is the missile?"))
+		print ("Don't fire the missile yet. It would be absolutly pointless.")
+else:
+	mi = float(input("So how many miles away is the missile? "))
 	distance = mi * 0.621371
-		if distance <= 3:
-			print ("Fire away! You would most likely hit it and avert disaster... today, anyway.")
-		else:
-			print ("Don't fire the missile yet. It would be absolutly pointless.")
+	if distance <= 3:
+		print ("Fire away! You would most likely hit it and avert disaster... today, anyway.")
+	else:
+		print ("Don't fire the missile yet. It would be absolutly pointless.")
 

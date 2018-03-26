5daeaa4e75786f958dce8306e1aeb560b17812e5
efalin <emiline.falin@gmail.com>
2018-03-26 08:38:16 -0400

    added 3 more commands

--- ai1.py
+++ ai1.py
@@ -5,7 +5,7 @@ def getCommand():
 	return command
 
 def listOfCommands():
-	print("Here is a list of commands you can use: Raise Shields, Fire Missiles, Exit Program")
+	print("Here is a list of commands you can use: Raise Shields, Fire Missiles, Contact Avengers, Contact SHIELD, Exit Program")
 
 def raiseShields(): 
 	print("The perimeter shields have been raised")
@@ -17,6 +17,12 @@ def exitProgram():
 	print("Aborting the mission now")
 	sys.exit(0)
 
+def contactAvengers():
+	print("The Avengers are being contacted. They will arrive in approximately four point three minutes")
+
+def contactSHIELD():
+	print("If you say so. SHIELD is being informed")
+
 
 while True:
 	command = getCommand()
@@ -27,5 +33,11 @@ while True:
 		fireMissiles()
 	elif command.lower() == "exit program":
 		exitProgram()
+	elif command.lower() == "contact avengers":
+		contactAvengers()
+	elif command.lower() == "contact shield":
+		contactSHIELD()
+	elif command.lower() == "options":
+		listOfCommands()
 	else:
 		print("Not understood")
